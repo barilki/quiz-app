df3a8aa0fde9edc25881f0fbf96c5980bb335420
barilki <djmmon@gmail.com>
2022-04-09 10:31:32 +0300

    Fix

--- src/components/quiz/Instructions.js
+++ src/components/quiz/Instructions.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component,Fragment} from 'react';
 import {Link} from 'react-router-dom';
 import {Helmet} from 'react-helmet';
 
--- src/components/quiz/Quiz.js
+++ src/components/quiz/Quiz.js
@@ -10,4 +10,4 @@ class Quiz extends React.Component{
 
 }
 
-export default Quiz;
+export default Quiz
