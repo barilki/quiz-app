99c24b9f546b7d77c92ff2d98b56dd9d29759fce
Bar-Ilan Kimbarovski <44907047+barilki@users.noreply.github.com>
2022-04-10 00:41:03 +0300

    Delete is-empty.js

--- src/utils/is-empty.js
+++
@@ -1,9 +0,0 @@
-import { objectSelectorString } from "materialize-css";
-
-const isEmpty = (value) => 
-value == undefined ||
-value == null || 
-(typeof value === 'object' && Object.keys(value).length==0)||
-(typeof value === 'string' && value.trim().length === 0);
-
-export default isEmpty;
