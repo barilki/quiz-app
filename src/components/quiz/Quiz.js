25f2680fee1d3ef86dd4c8c18090b611e8b97ff2
barilki <djmmon@gmail.com>
2022-04-09 16:46:38 +0300

    api

--- src/components/quiz/Quiz.js
+++ src/components/quiz/Quiz.js
@@ -1,21 +1,78 @@
-import React, {Component, Fragment} from "react";
+import React, {Fragment, useEffect} from "react";
 import {Helmet} from 'react-helmet';
+import isEmpty  from "../../utils/is-empty";
+
+const API_URL = "https://opentdb.com/api.php?amount=100";
+
 
 
 class Quiz extends React.Component{
-    // constructor(props){
-    //     super(props);
+    constructor(props){
+        super(props);
 
-    // }
-
-    increamentCounter = ()=>{
-        this.setState((prevState, props) => ({
-            counter: prevState.counter + 1
-        })); 
+        this.state = {
+            setQuestions: [],
+            questions: [],
+            currentQuestion: {},
+            nextQuestion: {},
+            previousQuestion:{},
+            answer:'',
+            numberOfQuestions: 0,
+            numberOfAnsweredQuestion: 0,
+            currentQuestionIndex: 0,
+            score:0,
+            correctAnswers:0,
+            wrongAnswers: 0,
+            hints: 5,
+            fifyFifty: 2,
+            usedFiftyFifty: false,
+            time: {},
+            DataisLoaded: false
+        };
     }
 
+    componentDidMount(){
+        fetch(API_URL).then((res) => res.json())
+        .then((json) => {
+            this.setState({
+                questions: json,
+                DataisLoaded: true
+            });
+        })
+        const{questions, currentQuestion,nextQuestion,previousQuestion} = this.state;
+        this.displayQuestions(questions,currentQuestion,nextQuestion,previousQuestion);
+
+    }
+
+    displayQuestions = (questions = this.state.questions, currentQuestion,nextQuestion,previousQuestion)=>{
+        let {currentQuestionIndex} = this.state; 
+        if(!isEmpty(this.state.questions)){
+            questions = this.state.questions;
+            currentQuestion = questions[currentQuestionIndex];
+            nextQuestion = questions[currentQuestionIndex + 1];
+            previousQuestion = questions[currentQuestionIndex -1];
+            const answer = currentQuestion.answer;
+            this.setState({
+                currentQuestion,
+                nextQuestion,
+                previousQuestion,
+                answer
+            })
+        }
+
+
+    
+   
+
+    };
+
+
     render(){
+
+        const {currentQuestion} = this.state;
+        console.log(currentQuestion);
         return(
+        
             <Fragment>
                 <Helmet><title>Quiz Page</title></Helmet>
                 <div className="questions">
@@ -34,7 +91,7 @@ class Quiz extends React.Component{
                            <span className="right">2:15<span className="mdi mdi-clock-outline mdi-24px"></span></span>
                         </p>
                     </div>
-                    <h5>Google was founded in what year?</h5>
+                    <h5>{currentQuestion.questions}</h5>
                     <div className="options-container">
                         <p className="options">1997</p>
                         <p className="options">1998</p>
---
+++ src/utils/is-empty.js
@@ -0,0 +1,9 @@
+import { objectSelectorString } from "materialize-css";
+
+const isEmpty = (value) => 
+value == undefined ||
+value == null || 
+(typeof value === 'object' && Object.keys(value).length==0)||
+(typeof value === 'string' && value.trim().length === 0);
+
+export default isEmpty;
