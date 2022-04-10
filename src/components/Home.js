53ebdf9d77754b411e7ca7ec67111701d08ff76b
Bar-Ilan Kimbarovski <44907047+barilki@users.noreply.github.com>
2022-04-10 12:06:57 +0300

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,10 +18,7 @@ export default function Home () {
                     <li><Link className='start-button' to="/play/instructions">Play</Link></li>
                 </ul>
             </div>    
-            <div className='auth-container'>
-                <Link to="/login" className='auth-buttons' id='login-button'>Login</Link>
-                <Link to="/register"className='auth-buttons' id='signup-button'>Register</Link>
-            </div>
+  
         </section>
       </div>
 
